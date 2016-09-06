76cb012c27a964b30a174f73986f634c61c51893
Jared Gray <jared@jaredgray.us>
2016-09-06 08:51:25 -0700

    Completion of Assignment 2

--- Week4/GRAY_A04Q02.java
+++ Week4/GRAY_A04Q02.java
@@ -1,3 +1,14 @@
+/**
+ * Assignment 4 Question 2: Capture user input of 5 numbers and then display the average.
+ * 													Program only accepts numbers and warns the user if an invalid
+ * 													character is entered.
+ *   
+ * Completion time: 45 minutes
+ * 
+ * @author  Jared Gray
+ * @version 1.0
+ */
+
 package Week4;
 
 import java.util.Scanner;
@@ -5,6 +16,7 @@ import java.util.Scanner;
 public class GRAY_A04Q02 {
 	static final int MAX_SIZE = 5;
 	static double[] values = new double[MAX_SIZE];
+	static double sum = 0;
 	
 	public static void main(String[] args) {
 		userInput();
@@ -14,13 +26,19 @@ public class GRAY_A04Q02 {
 		Scanner scan = new Scanner(System.in);
 		
 		for (int i = 0; i < values.length; i++) {
-			try {
-				System.out.println("Please enter a number: ");
-				values[i] = Double.parseDouble(scan.nextLine());
-			} catch (java.lang.NumberFormatException e) {
-				System.out.println("Please enter a valid number.");
-				continue;
+			while(true) {
+				try {
+					System.out.println("Please enter a number: ");
+					values[i] = Double.parseDouble(scan.nextLine());
+					sum = sum + values[i];
+					break;
+				} catch (java.lang.NumberFormatException e) {
+					System.out.println("Please enter a valid number.");
+					continue;
+				}
 			}
 		}
+		double average = sum / MAX_SIZE;
+		System.out.println("The average is: " + average);
 	}
 }
